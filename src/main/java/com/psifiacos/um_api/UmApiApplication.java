d018c1785e9189f4f4e9d0faa9b566d27f0587b6
Ernesto Ojeda Arias <eojeda@gmx.es>
2023-04-18 17:31:55 -0400

    Initial commit

---
+++ src/main/java/com/psifiacos/um_api/UmApiApplication.java
@@ -0,0 +1,13 @@
+package com.psifiacos.um_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UmApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/psifiacos/um_api/UmApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psifiacos.um_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
