193138ebe30271de157b08d07354c00dd051f2b7
Ernesto <eojeda@gmx.es>
2023-07-12 12:33:44 -0400

    Fixing find units filter

--- src/main/java/com/psifiacos/um_api/dtos/UnitDto.java
+++ src/main/java/com/psifiacos/um_api/dtos/UnitDto.java
@@ -1,6 +1,5 @@
 package com.psifiacos.um_api.dtos;
 
-import com.psifiacos.um_api.model.documents.Unit;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Builder;
